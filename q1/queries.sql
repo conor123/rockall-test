18c577a21ad6646a1e1c5d4d47ece8d3597f7a1f
Conor <office@conorryan.ie>
2017-12-18 21:13:33 +0000

    type

--- q1/queries.sql
+++ q1/queries.sql
@@ -3,7 +3,7 @@ SELECT * FROM users;
 #
 SELECT * FROM training_details;
 # q1 
-# Write a query to get the list of users who took the a training lesson more than once in the same day, 
+# Write a query to get the list of users who took a training lesson more than once in the same day, 
 # grouped by user and training lesson, 
 # each ordered from the most recent lesson date to oldest date.
 SELECT u.user_id, u.username, STR_TO_DATE(td.training_date, '%Y-%m-%d') as training_date, td.training_id, count(td.training_id) as tids
